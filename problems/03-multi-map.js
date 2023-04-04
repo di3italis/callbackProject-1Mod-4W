5b897c45a7f4976e373d09aa7b7798393915b183
takashi <takashi.sh@proton.me>
2023-04-04 19:14:58 -0400

    problem 03 switch driver

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -5,29 +5,36 @@ through the callback n times.
 
 Examples:
 
-let result1 = multiMap(7, 2, function(n) {
-  return n * 10;
+*******************************************************************************/
+
+function multiMap(val, n, cb) {
+    let ans = 0;
+    let loop = val;
+    for (i = 0; i < n; i++) {
+        // ans += cb(val);
+        loop += cb(loop);
+    }
+    return loop;
+}
+
+let result1 = multiMap(7, 2, function (n) {
+    return n * 10;
 });
 console.log(result1); // 700
 
-let result2 = multiMap(7, 3, function(n) {
+let result2 = multiMap(7, 3, function (n) {
     return n * 10;
 });
 console.log(result2); // 7000
 
-let result3 = multiMap("hi", 5, function(s) {
-  return s + "!";
+let result3 = multiMap("hi", 5, function (s) {
+    return s + "!";
 });
 console.log(result3); // hi!!!!!
-*******************************************************************************/
-
-function multiMap(val, n, cb) {
-  // Your code here
-}
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
-  module.exports = multiMap;
-} catch(e) {
-  return null;
+    module.exports = multiMap;
+} catch (e) {
+    return null;
 }
